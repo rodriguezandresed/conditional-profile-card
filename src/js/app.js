59faf8ab4564cc101008ece79e48a0c50e93bdde
Andres Rodriguez <rodriguezandresed@gmail.com>
2022-04-02 19:32:29 -0400

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -30,22 +30,37 @@ function render(variables = {}) {
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
+  // de aca
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name == null ? "" : variables.name}</h1>
+          <h2>${variables.role == null ? "" : variables.role}</h2>
+          <h3>${variables.city == null ? "" : variables.city + ", "} ${
+    variables.country == null ? "" : variables.country
+  }</h3>
+          <ul class="${
+            variables.socialMediaPosition == null
+              ? ""
+              : variables.socialMediaPosition
+          }">
+            <li><a href="https://twitter.com/${
+              variables.twitter == null ? "" : variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github == null ? "" : variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin == null ? "" : variables.linkedin
+            }"><i class="fab fa-linkedin"> </i></a></li>
+            <li><a href="https://instagram.com/ ${
+              variables.instagram == null ? "" : variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
 }
-
+// a aca tengo que modificar para el ejercicio
 /**
  * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
